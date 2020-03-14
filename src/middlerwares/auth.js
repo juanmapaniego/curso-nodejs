0451170d74bfcbaa6231c0f5792bbbdd5b18f778
jmpaniego <juanma_paniego@hotmail.com>
2020-03-14 20:32:42 -0300

    added users routes

---
+++ src/middlerwares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const isAuth = (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET);
+      next();
+    } else {
+      throw {
+        code: 403,
+        status: "ACCESS_DENIED",
+        message: "Missing header token"
+      };
+      /* res
+        .status(403)
+        .json({ status: "ACCESS_DENIED", message: "Missin header token" }); */
+    }
+  } catch (error) {
+    res
+      .status(error.code || 500)
+      .json({ status: error.status || "ERROR", message: error.message });
+  }
+};
+
+module.exports = { isAuth };
--- src/routes/v1/users-routes.js
+++ src/routes/v1/users-routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
+const { isAuth } = require("../../middlerwares/auth.js");
 const usersController = require("../../controllers/v1/users-controller.js");
 
 const router = express.Router();
 router.post("/create", usersController.createUser);
-router.post("/update", usersController.updateUser);
+router.post("/update",isAuth, usersController.updateUser);
 router.post("/delete", usersController.deleteUser);
 router.post("/get-all", usersController.getUsers);
-router.post('/login', usersController.login);
+router.post("/login", usersController.login);
 
 module.exports = router;
